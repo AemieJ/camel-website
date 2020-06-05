5f2f2df0b748209800baaaa5f8377739b6548576
Aemie <aemie.j@hotmail.co.uk>
2020-06-05 11:54:54 +0530

    use different search layout as per screen width

--- antora-ui-camel/src/js/06-nav-small-search.js
+++ antora-ui-camel/src/js/06-nav-small-search.js
@@ -17,7 +17,7 @@
     'nav-burger': document.getElementsByClassName('navbar-burger')[0],
     'search-cancel': document.getElementById('search-cancel'),
     'search-result': document.getElementById('search_results'),
-    search: document.getElementById('search'),
+    'search': document.getElementById('search'),
     'nav-menu': document.getElementsByClassName('navbar-menu')[0],
     'nav-search': document.getElementsByClassName('navbar-search')[0],
   }
@@ -25,7 +25,6 @@
   var search = document.getElementsByClassName('search-mobile')[0]
   var navigator = document.getElementById('navigate-searchbar')
   var navWidth = document.getElementsByClassName('navbar')[0].offsetWidth
-  var condition = navWidth < 1024
 
   function toggleMenuToSearch () {
     states['navbar-search'] = states['navigate-searchbar'] = true
@@ -41,31 +40,23 @@
       domHandles['nav-tools'].style.display = 'none'
       domHandles['nav-burger'].style.display = 'none'
       domHandles['search-cancel'].style.bottom = 'calc(50% - 0.05rem)'
-      domHandles['search'].style.display = navigator.style.display = 'block'
-      domHandles['search-result'].style.width = domHandles['search'].style.width = (0.85 * navWidth).toString() + 'px'
-      if (navWidth >= 1024 && navWidth <= 1220) {
-        domHandles['nav-menu'].style.display = 'none'
-        domHandles['nav-search'].style.display = 'block'
-        domHandles['search-result'].style.width = domHandles['search'].style.width = (0.92 * navWidth).toString() + 'px'
-      } else if (navWidth >= 565) {
-        domHandles['search-result'].style.width = domHandles['search'].style.width = (0.9 * navWidth).toString() + 'px'
-      }
+      domHandles["search"].style.display = navigator.style.display = "block";
+      domHandles['nav-menu'].style.display = "none";
+      domHandles["nav-search"].style.display = "block";
+      domHandles["search-result"].style.width = domHandles["search"].style.width = (0.92 * navWidth).toString() + "px";
+
     } else {
       domHandles['nav-logo'].style.display = domHandles['nav-fill'].style.display = 'block'
       domHandles['nav-tools'].style.display = 'inline-flex'
       domHandles['search-cancel'].style.display = domHandles['search'].style.display = 'none'
       domHandles['search'].value = ''
-      navigator.style.display = 'none'
-      if (condition) {
-        domHandles['nav-burger'].style.display = 'block'
-      } else {
-        domHandles['nav-menu'].style.display = 'inline-flex'
-        domHandles['nav-search'].style.display = 'none'
-      }
+      navigator.style.display = "none";
+      domHandles["nav-menu"].style.display = "inline-flex";
+      domHandles["nav-search"].style.display = "none";
     }
   }
 
-  if (navWidth <= 1220) {
+  if (navwidth >= 1024 && navWidth <= 1220) {
     search.addEventListener('click', function () {
       toggleMenuToSearch()
       handleDOMStyle()
@@ -74,5 +65,8 @@
       toggleSearchToMenu()
       handleDOMStyle()
     })
+  }
+  if (navWidth < 1024) { 
+    document.getElementsByClassName('within-nav')[0].style.width = document.getElementsByClassName('navbar-end')[0].offsetWidth
   }
 })()
